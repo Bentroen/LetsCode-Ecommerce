60d5d626e76c622c30942b50578f529f0737d6d1
Bentroen <bemcdc@gmail.com>
2022-05-30 04:45:04 -0300

    Add `@ComponentScan` to application class

--- src/main/java/br/com/letscode/produtoapp/EcommerceProdutoAppApplication.java
+++ src/main/java/br/com/letscode/produtoapp/EcommerceProdutoAppApplication.java
@@ -1,9 +1,12 @@
 package br.com.letscode.produtoapp;
 
+import br.com.letscode.produtoapp.controller.ProdutoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = ProdutoController.class)
 public class EcommerceProdutoAppApplication {
 
     public static void main(String[] args) {
