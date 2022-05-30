b9d585cf8ea21ef4d160fb613ca151057b4009db
Bentroen <bemcdc@gmail.com>
2022-05-29 21:08:29 -0300

    Initial commit

---
+++ src/main/java/br/com/letscode/produtoapp/EcommerceProdutoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.produtoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProdutoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProdutoAppApplication.class, args);
+    }
+
+}
