d711bb279ba49f674dbf8f5b8f3ff77c0f011e1f
Bentroen <bemcdc@gmail.com>
2022-06-01 13:08:38 -0300

    Add base project for VendaApp

---
+++ venda-app/src/main/java/br/com/letscode/vendaapp/VendaAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.vendaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaAppApplication.class, args);
+	}
+
+}
